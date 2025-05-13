27b80af104d06850f981a37ecc941ca25ffdafba
abderrahmane <abderrahmanelaajili0@gmail.com>
2025-05-13 19:43:10 +0100

    feat: add the jwt configuration

---
+++ src/Entity/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: '`user`')]
+#[ApiResource]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
